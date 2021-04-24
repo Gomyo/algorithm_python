a6f49db13ca3ca32974f17df45438f9ae7d1016e
최준원 <cjw1102@naver.com>
2021-04-24 20:12:29 +0900

    basic

--- 하나로.py
+++ 하나로.py
@@ -9,24 +9,20 @@ def union(x,y):
     a = find_set(x)
     b = find_set(y)
 
-    if a == b:
-        return
+    if a > b:
+        p[a] = b
     else:
-        if x > y:
-            p[x] = y
-        else:
-            p[y] = x
+        p[b] = a
 
 def kruscal(data):
     result = 0
-    # count = 0
 
     while data:
-        a,b,d = heapq.heappop(data)
+        d, a, b = heapq.heappop(data)
         if find_set(a) == find_set(b): continue
         result += d
         union(a,b)
-        # count+=1
+
     return result
 
 T = int(input())
@@ -43,7 +39,7 @@ for tc in range(1, T+1):
     for i in range(N):
         for j in range(i+1, N):
             distance = ((x[i] - x[j])**2 + (y[i] - y[j])**2)*E
-            heapq.heappush(data, (i, j, distance))
+            heapq.heappush(data, (distance, i, j))
 
     res = int(kruscal(data) + 0.5)
     print('#{} {}'.format(tc, res))
