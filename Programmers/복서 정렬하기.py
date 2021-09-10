f10457e8c8fd8ec51fd6e7dd6d0bb5f9988b10d5
최준원 <cjw1102@naver.com>
2021-09-11 00:35:34 +0900

    복서 정렬하기

--- Programmers/복서 정렬하기.py
+++ Programmers/복서 정렬하기.py
@@ -5,14 +5,18 @@ def solution(weights, head2head):
         n = head2head[i].count('N')
         w = head2head[i].count('W')
         l = head2head[i].count('L')
-        winrate = round(w/(w+l)*100, 2)
+        if n != len(weights):
+            winrate = w/(w+l)
+        else:
+            winrate = 0
         heavier = 0
         for j in range(len(weights)):
-            if weights[i] != weights[j]:
-                if weights[i] < weights[j] and head2head[i][j] == 'W':
-                    heavier += 1
+            if weights[i] < weights[j] and head2head[i][j] == 'W':
+                heavier += 1
         cal.append((winrate, heavier, weights[i], i+1))
-    answer = sorted(cal, key=lambda x: (-x[0], -x[1], x[2], x[3]))
+    answer = sorted(cal, key=lambda x: (-x[0], -x[1], -x[2], x[3]))
+    answer = [x[3] for x in answer]
     return answer
 
-solution([50,82,75,120], ["NLWL","WNLL","LWNW","WWLN"])
+# solution([50,82,75,120], ["NLWL","WNLL","LWNW","WWLN"])
+solution([145, 92, 86], ["NWW", "LNW", "LLN"])
