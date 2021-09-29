e2690de9fe321b18362e4b787edece2f4f250b53
최준원 <cjw1102@naver.com>
2021-09-29 22:44:44 +0900

    solve

--- Programmers/최소직사각형.py
+++ Programmers/최소직사각형.py
@@ -1,10 +1,15 @@
+# def solution(sizes):
+#     h, l = 0, 0
+#     for size in sizes:
+#         if min(size) > l:
+#             l = min(size)
+#         if max(size) > h:
+#             h = max(size)
+#     return l*h
+
+# 위 로직을 한줄로
 def solution(sizes):
-    h, l = 0, 0
-    for size in sizes:
-        if min(size) > l:
-            l = min(size)
-        if max(size) > h:
-            h = max(size)
-    return l*h
+    return max(max(x) for x in sizes) * max(min(x) for x in sizes)
 
 print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
+
